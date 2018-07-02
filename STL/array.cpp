035e99cd60a4e47540d21e8788617439119bdc17
0x000N3X4N <38838465+0x000N3X4N@users.noreply.github.com>
2018-07-03 00:39:41 +0300

    Update array.cpp

--- STL/array.cpp
+++ STL/array.cpp
@@ -8,41 +8,105 @@ namespace D34D {
 	{
 	public:
 		T arr[N];
-		inline T size();
-		inline T at(int);
-		inline T front();
-		inline T back();
-		inline T* data();
+		typedef T* iterator;
+		typedef _STD  reverse_iterator<iterator> reverse_iterator;
+		inline constexpr T size() const noexcept;
+		inline constexpr T at(int) const;
+		inline constexpr T front() const;
+		inline constexpr T back() const;
+		inline iterator data();
 		inline bool empty();
-		std::_Array_iterator<T, N> begin();
-		std::_Array_iterator<T, N> end();
+		inline constexpr T max_size() const noexcept;
+		void swap(array<T, N> &obj) noexcept;
+		void fill(const T& value);
+		iterator begin();
+		constexpr iterator cbegin() const;
+		reverse_iterator rbegin();
+		iterator end();
+		constexpr iterator cend() const;
+		reverse_iterator rend();
 		T &operator [](int);
 	};
+
 	template <size_t ID, class T, size_t N>
 	T &get(array<T, N> &obj) {
 		return (obj.arr[ID]);
+	}
+
+	template <class T, size_t N>
+	bool operator==(array <T, N> &lft, array<T, N> &rght) {
+		for (size_t i = 0; i < N; i++) {
+			if (lft[i] != rght[i])
+				return false;
+		}
+		return true;
+	}
+
+	template <class T, size_t N>
+	constexpr bool operator != (array <T, N> &lft, array<T, N> &rght) {
+		return !(lft == rght);
+	}
+
+	template <class T, size_t N>
+	bool operator < (array <T, N> &lft, array<T, N> &rght) {
+		for (size_t i = 0; i < N; i++) {
+			if (lft[i] < rght[i])
+				return true;
+			if (rght[i] < lft[i])
+				return false;
+		}
+	}
+
+	template <class T, size_t N>
+	bool operator <= (array <T, N> &lft, array<T, N> &rght) {
+		for (size_t i = 0; i < N; i++) {
+			if (lft[i] <= rght[i])
+				return true;
+			if (rght[i] <= lft[i])
+				return false;
+		}
+	}
+
+	template <class T, size_t N>
+	bool operator > (array <T, N> &lft, array<T, N> &rght) {
+		for (size_t i = 0; i < N; i++) {
+			if (lft[i] > rght[i])
+				return true;
+			if (rght[i] > lft[i])
+				return false;
+		}
+	}
+
+	template <class T, size_t N>
+	bool operator >= (array <T, N> &lft, array<T, N> &rght) {
+		for (size_t i = 0; i < N; i++) {
+			if (lft[i] >= rght[i])
+				return true;
+			if (rght[i] >= lft[i])
+				return false;
+		}
 	}
 }
 
 using namespace	D34D;
 
 template <class T, size_t N>
-inline T array<T, N>::size() {
+inline constexpr T array<T, N>::size() const noexcept {
 	return N;
 }
 
 template <class T, size_t N>
-inline T array <T, N>::at(int n) {
+inline constexpr T array <T, N>::at(int n) const {
 	return arr[n];
 }
 
 template <class T, size_t N>
-inline T array<T, N>::front() {
+inline constexpr T array<T, N>::front() const{
 	return arr[0];
 }
 
 template <class T, size_t N>
-inline T array<T, N>::back() {
+inline constexpr T array<T, N>::back() const {
 	return arr[N-1];
 }
 
@@ -52,7 +116,7 @@ T &array<T, N>::operator[](int n) {
 }
 
 template <class T, size_t N>
-T *array<T, N>::data() {
+typename array<T, N>::iterator array<T, N>::data() {
 	return arr;
 }
 
@@ -62,29 +126,83 @@ inline bool array<T, N>::empty() {
 }
 
 template <class T, size_t N>
-std::_Array_iterator<T, N> array<T, N>::begin() {
-	return (std::_Array_iterator<T, N>(_STD addressof(arr[0]), 0));
+typename array<T, N>::iterator array<T, N>::begin() {
+	return arr;
 }
 
 template <class T, size_t N>
-std::_Array_iterator<T, N> array<T, N>::end() {
-	return (std::_Array_iterator<T, N>(_STD addressof(arr[0]), N));
+typename array<T, N>::iterator array<T, N>::end() {
+	return arr + N;
+}
+
+template <class T, size_t N>
+void array <T, N>::swap(array<T, N> &obj) noexcept {
+	for (size_t i = 0; i < N; i++)
+	{
+		T tmp = arr[i];
+		T tmp1 = obj.arr[i];
+		obj.arr[i] = tmp;
+		arr[i] = tmp1;
+	}	
+}
+
+template <class T, size_t N>
+inline constexpr T array<T, N>::max_size() const noexcept {
+	return N;
+}
+
+template <class T, size_t N>
+constexpr typename array<T, N>::iterator array<T, N>::cbegin() const {
+	return (begin());
+}
+
+template <class T, size_t N>
+constexpr typename array<T, N>::iterator array<T, N>::cend() const {
+	return (end());
+}
+
+template <class T, size_t N>
+typename array<T, N>::reverse_iterator array<T, N>::rbegin() {
+	return (reverse_iterator(end()));
+}
+
+template <class T, size_t N>
+typename array<T, N>::reverse_iterator array<T, N>::rend() {
+	return (reverse_iterator(begin()));
+}
+
+template <class T, size_t N>
+void array<T, N>::fill(const T& value) {
+	for (size_t i = 0; i < N; i++)
+		arr[i] = value;
+	
 }
 
 int main() {
-	array<double, 5> af = { 1.3, 2, 3, 4, 5 };
-	std::cout << "size = " << af.size() << '\n';
-	std::cout << "at = " << af.at(4) << '\n';
-	std::cout << "sum across at() = " << af.at(3) + af.at(1) << '\n';
-	std::cout << "array front() = " << af.front() << '\n';
-	std::cout << "array back() = " << af.back() << '\n';
-	std::cout << af[0] << '\n';
-	
-	std::cout << af[0] << '\n';
-	std::cout << af.data() << '\n';
-	std::cout << af.empty() << '\n';
-	std::copy(af.begin(), af.end(), std::ostream_iterator<double>(std::cout, " "));
-	std::cout << '\n' << get<1>(af) << '\n';
+	array<double, 5> af = { 3, 2, 3, 4, 5 };
+	array<double, 5> df = { 8, 4.3, 2.3, 12, 25 };
+	if (af > df)
+		_STD cout << "true\n";
+	_STD cout << "size = " << af.size() << '\n';
+	_STD cout << "at = " << af.at(4) << '\n';
+	_STD cout << "sum across at() = " << af.at(3) + af.at(1) << '\n';
+	_STD cout << "array front() = " << af.front() << '\n';
+	_STD cout << "array back() = " << af.back() << '\n';
+	_STD cout << af[0] << '\n';
+	af[0] += df[0];
+	_STD cout << af.data() << '\n';
+	_STD cout << af.empty() << '\n';
+	_STD copy(af.begin(), af.end(), _STD ostream_iterator<double>(_STD cout, " "));
+	_STD cout << '\n' << get<1>(af) << '\n';
+	df.swap(af);
+	_STD copy(af.begin(), af.end(), _STD ostream_iterator<double>(_STD cout, " "));
+	_STD  cout << '\n';
+	_STD copy(df.begin(), df.end(), _STD ostream_iterator<double>(_STD cout, " "));
+	_STD  cout << "\nreverse array: ";
+	_STD copy(df.rbegin(), df.rend(), _STD ostream_iterator<double>(_STD cout, " "));
+	_STD  cout << "\nmax_size = " << af.max_size() << '\n';
+	af.fill(5);
+	_STD copy(af.begin(), af.end(), _STD ostream_iterator<double>(_STD cout, " "));
 	system("pause");
 	return 0;
 }
